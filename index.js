398421ac43882350a817d9a4e51c39887e165413
Sherobod <sherobodish@gmail.com>
2024-06-18 17:13:02 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+
+
+// //a 
+// const body = document.body
+// const square = document.createElement('div')
+// const link = document.createElement('a')
+// const text = document.createElement('span')
+
+// // b 
+// square.classList.add('square')
+// link.href = "#"
+// text.innerText = "1878$"
+
+
+// link.innerText = "Nicolas Riviera"
+
+// // c 
+// square.append(link, text)
+
+
+
+
+// body.prepend(square)
+
+
+
+// // a
+// const body = document.body
+// const rec = document.createElement('div')
+// const span = document.createElement('span')
+// const inp = document.createElement('input')
+
+
+// // b 
+// rec.classList.add('rec')
+// span.innerText = "Alan Adams"
+// inp.placeholder = ('Age')
+// inp.type = ('text')
+// inp.name = ('age')
+
+// // c
+// body.prepend(rec)
+// rec.append(span, inp)
+
+
+
+
+
+
+// // a 
+// const body = document.body
+// const mens_clothes = document.createElement('div')
+// const bag = document.createElement('div')
+// const bag_img = document.createElement('img')
+// const clothing = document.createElement('div')
+// const men_clothing = document.createElement('div')
+// const p_mens = document.createElement('p')
+// const perfect = document.createElement('div')
+// const p_perfect = document.createElement('p')
+// const prices = document.createElement('div')
+// const price = document.createElement('div')
+// const img_dollar = document.createElement('img')
+// const span_dollar = document.createElement('span')
+// const img_star = document.createElement('img')
+// const span_star = document.createElement('span')
+// const img_box = document.createElement('img')
+// const span_box = document.createElement('span')
+// const button = document.createElement('button')
+
+
+// // b 
+// mens_clothes.classList.add('mens_clothes')
+// bag.classList.add('bag')
+// bag_img.src = "./img/bag.jpg"
+// bag_img.alt = "суька"
+// clothing.classList.add('clothing')
+// men_clothing.classList.add('men_clothing')
+// men_clothing.innerText = "MEN’S CLOTHING (120)"
+// perfect.classList.add('perfect')
+// perfect.innerText = "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
+// prices.classList.add('prices')
+// price.classList.add('price')
+// img_dollar.src = "./img/1.png"
+// img_dollar.alt = "dollar"
+// span_dollar.innerText = "109"
+// img_star.src = "./img/2.png"
+// img_star.alt = "star"
+// span_star.innerText = "3,9"
+// img_box.src = "./img/3.png"
+// img_box.alt = "box"
+// span_box.innerText = "120"
+// button.innerText = "В избранное"
+
+// // c
+// body.prepend(mens_clothes)
+// mens_clothes.append(bag, clothing)
+// bag.append(bag_img)
+// clothing.append(men_clothing, perfect, prices, button)
+// prices.append(price)
+// price.append(img_dollar, span_dollar, img_star,  span_star, img_box,  span_box)
